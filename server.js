ac04dac49390d39441bc0cf8c6164cc7511315a4
keviveks <keviveks@gmail.com>
2016-05-02 23:05:24 +0530

    DELETE todos route

--- server.js
+++ server.js
@@ -43,6 +43,19 @@ app.post('/todos', function(req, res) {
 	res.send(body);
 });
 
+app.delete('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(matchedTodo) {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	} else {
+		res.status(404).json({"error": "No todo found with that id!"});
+	}
+	
+});
+
 app.listen(PORT, function() {
 	console.log("Server Listen to Port: " + PORT);
 });
