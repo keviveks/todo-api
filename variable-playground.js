17aa4d28817b2e51c1c27f3fce1cfb042e61040c
keviveks <keviveks@gmail.com>
2016-05-17 23:25:16 +0530

    Variable play ground

---
+++ variable-playground.js
@@ -0,0 +1,14 @@
+var grades = [12, 34];
+
+function addGrade(arr) {
+	arr = [12, 34, 45];
+}
+
+function pushGrades(arr) {
+	arr.push(45);
+	debugger;
+}
+
+// addGrade(grades);
+pushGrades(grades);
+console.log(grades);
